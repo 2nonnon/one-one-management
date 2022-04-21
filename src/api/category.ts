74c6dbea8ddb122779adf21cd90afbe7f7951d5e
2nonnon <lanpangzi0205@gmail.com>
2022-04-21 16:18:00 +0800

    feat: 封装商品分类请求

---
+++ src/api/category.ts
@@ -0,0 +1,25 @@
+import { ICategories, CreateCategoryDto, ICategory } from './types/category'
+import BaseHttpService from '../utils/base-http.service'
+
+class CategoryService extends BaseHttpService {
+  async getCategories () {
+    const res = await this.get<ICategories>('categories')
+    return res.data
+  }
+
+  async createCategory (data: CreateCategoryDto) {
+    const res = await this.post<ICategory>('categories', data)
+    return res.data
+  }
+
+  async deleteCategory (id: number) {
+    await this.delete<void>(`categories/${id}`)
+  }
+
+  async updateCategory (data: CreateCategoryDto) {
+    const res = await this.post<ICategory>('categories', data)
+    return res.data
+  }
+}
+
+export const categoryService = new CategoryService()
