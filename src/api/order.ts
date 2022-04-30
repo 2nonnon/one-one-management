77a70ad52864d56895feee84b4860e21685b7c34
2nonnon <lanpangzi0205@gmail.com>
2022-04-30 16:09:15 +0800

    feat: 新建订单类型文件

---
+++ src/api/order.ts
@@ -0,0 +1,28 @@
+import BaseHttpService from '../utils/base-http.service'
+import { GetOrdersPageDto, IOrder, IOrdersPage, UpdateOrderReceiveInfoDto, UpdateOrderStatusDto } from './types/order.type'
+
+class OrderService extends BaseHttpService {
+    private BASE_URL = 'orders/admin'
+
+    async getOrderById (id: string) {
+      const res = await this.get<IOrder>(`${this.BASE_URL}/${id}`)
+      return res.data
+    }
+
+    async getOrdersPage (data: GetOrdersPageDto) {
+      const res = await this.post<IOrdersPage>(`${this.BASE_URL}/page`, data)
+      return res.data
+    }
+
+    async updateOrderStatus (id: string, data: UpdateOrderStatusDto) {
+      const res = await this.patch<IOrder>(`${this.BASE_URL}/${id}/status`, data)
+      return res.data
+    }
+
+    async updateOrderReceiveInfo (id: string, data: UpdateOrderReceiveInfoDto) {
+      const res = await this.patch<IOrder>(`${this.BASE_URL}/${id}/receive`, data)
+      return res.data
+    }
+}
+
+export const orderService = new OrderService()
