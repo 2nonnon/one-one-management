385293536cc4f72372f40919e7a4a25ea739132e
2nonnon <lanpangzi0205@gmail.com>
2022-04-30 16:10:47 +0800

    feat: 初始化订单管理页面

--- src/views/order/list/index.vue
+++ src/views/order/list/index.vue
@@ -4,7 +4,29 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, reactive, ref } from 'vue'
+import { orderService } from '../../../api/order'
+import { GetOrdersPageDto, IOrder } from '../../../api/types/order.type'
+
+const data = reactive<IOrder[]>([])
+const currentPage = ref(1)
+const pageSize = ref(12)
+const total = ref(0)
+
+const load = (options: GetOrdersPageDto) => {
+  orderService.getOrdersPage(options).then((res) => {
+    data.length = 0
+    total.value = res.total
+    data.push(...res.orders)
+    console.log(data)
+  })
+}
+
+onMounted(() => {
+  load({ current_page: currentPage.value, page_size: pageSize.value })
+})
+</script>
 
 <style scoped lang="scss">
 .title {
